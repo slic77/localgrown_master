989c1023c2d1e574965b6d91315bcb4c72c48ff5
chris <sharc06@gmail.com>
2017-11-15 11:43:22 -0600

    Removed permission decorators from class views as they are no longer needed since we are using generics. Placed the permissions within the classes themselves.

--- lgc_wep_api/views.py
+++ lgc_wep_api/views.py
@@ -1,18 +1,17 @@
 
 from lgc_wep_api.models import Users
 from lgc_wep_api.serializers import UsersSerializer
-from rest_framework.decorators import permission_classes
 from rest_framework import permissions
 from rest_framework import generics
 
 
-@permission_classes((permissions.AllowAny,))
 class UserList(generics.ListCreateAPIView):
+    permission_classes = (permissions.AllowAny,)
     queryset = Users.objects.all()
     serializer_class = UsersSerializer
 
 
-@permission_classes((permissions.AllowAny,))
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.AllowAny,)
     queryset = Users.objects.all()
     serializer_class = UsersSerializer

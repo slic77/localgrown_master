44b2a4250c059938cd77b584923280f1bdcc9f48
chris <sharc06@gmail.com>
2017-11-15 11:31:09 -0600

    Wow. Following the django REST framework tutorial. Really cleaned up the code. Looks nice and DRY.

--- lgc_wep_api/views.py
+++ lgc_wep_api/views.py
@@ -1,62 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+
 from lgc_wep_api.models import Users
 from lgc_wep_api.serializers import UsersSerializer
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.response import Response
-from rest_framework import status, permissions
-from rest_framework.views import APIView
-from django.http import Http404
+from rest_framework.decorators import permission_classes
+from rest_framework import permissions
+from rest_framework import generics
 
 
 @permission_classes((permissions.AllowAny,))
-class UserList(APIView):
-    """
-    List all users, or create a new user
-    """
-
-    def get(self, request, format=None):
-        users = Users.objects.all()
-        serializer = UsersSerializer(users, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = UsersSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class UserList(generics.ListCreateAPIView):
+    queryset = Users.objects.all()
+    serializer_class = UsersSerializer
 
 
 @permission_classes((permissions.AllowAny,))
-class UserDetail(APIView):
-    """
-    Retreive, update, or delete a user
-    """
-
-    def get_object(self, pk):
-        try:
-            return Users.objects.get(pk=pk)
-        except Users.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        user = self.get_object(pk)
-        serializer = UsersSerializer(user)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        user = self.get_object(pk)
-        serializer = UsersSerializer(user, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        user = self.get_object(pk)
-        user.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Users.objects.all()
+    serializer_class = UsersSerializer
